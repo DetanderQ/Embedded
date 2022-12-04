dfaeb2d2a9d5664a86e4dea33fb4185c74279e25
Pavlo <lezheninpasha@gmail.com>
2022-12-04 18:24:43 +0200

    Add Task1

---
+++ task1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("group1\n");
+	printf("Lezhenin Pavlo Daniilovich\n");
+	printf("lezheninpasha@gmail.com\n");
+	printf("https://gitlab.com/Detander/gl-embedded\n");
+	printf("https://drive.google.com/drive/folders/1Ogb60rpcsmjJzAP6yXUPJ-yWmqe3mVsg\n");                 
+	return 0;
+}
